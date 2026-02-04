76e73bfff35db006417564a99c215425c1acf5aa
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-04 21:47:04 +0500

    CReate the context provide as well and then import it and use it as wrapper in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <h1>
         React and Context API
       </h1>
-    </>
+    </UserContextProvider>
   )
 }
 
