55b238ea034b3cdda0b3e82c62fce508286d80a8
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-05 18:44:36 +0500

    Call and write the profile and login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <h1>
         React and Context API
       </h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
